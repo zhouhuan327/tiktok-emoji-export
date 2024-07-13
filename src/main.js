b34728854cb51137389826bcc9bbb4c1e1c55972
zhouhuan <zsean21598@163.com>
2024-07-13 20:14:45 +0800

    feat: init

---
+++ src/main.js
@@ -0,0 +1,42 @@
+
+
+async function downloadImagesAsZip() {
+  const imgList = Array.from(
+    document.querySelector('.emoji-card-outer-container')?.children[0]?.children[2]?.querySelectorAll("img") || []
+  ).map(item => item.src)
+  console.log("表情数量", imgList.length)
+  if(!imgList.length) {
+    console.log("未找到表情,请确认是否打开表情包弹窗")
+    return;
+  }
+  
+  // Dynamically insert JSZip script
+  const jsZipScript = document.createElement('script');
+  jsZipScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.6.0/jszip.min.js';
+  document.head.appendChild(jsZipScript);
+
+  jsZipScript.onload = async () => {
+    const zip = new JSZip();
+
+    const promises = imgList.map(async (src, index) => {
+      const response = await fetch(src);
+      const blob = await response.blob();
+      const type = response.headers.get('content-type')?.includes("gif") ? 'gif' : 'png'
+      zip.file(`image${index + 1}.${type}`, blob);
+    });
+
+    await Promise.all(promises);
+
+    zip.generateAsync({ type: "blob" }).then(function (content) {
+      const a = document.createElement("a");
+      const url = window.URL.createObjectURL(content);
+      a.href = url;
+      a.download = "抖音表情包导出.zip";
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(url);
+      document.body.removeChild(a);
+    });
+  };
+}
+downloadImagesAsZip()
